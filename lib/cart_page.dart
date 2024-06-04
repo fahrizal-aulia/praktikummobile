830de64f54b34c9deea24ef1e91247df580f6fd0
fahrizal-aulia <fahrizalaulia@gmail.com>
2024-06-04 19:48:28 +0700

    snackbar delete

--- lib/cart_page.dart
+++ lib/cart_page.dart
@@ -28,8 +28,58 @@ class CartPage extends StatelessWidget {
                   title: Text(cart.cart[index].name),
                   subtitle: Text(
                       '\$${cart.cart[index].price} x ${cart.cart[index].quantity}'),
-                  trailing: Text(
-                      'Total: \$${cart.cart[index].price * cart.cart[index].quantity}'),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                          'Total: \$${cart.cart[index].price * cart.cart[index].quantity}'),
+                      IconButton(
+                        icon: Icon(Icons.delete),
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: Text('Confirm Deletion'),
+                                content: Text(
+                                    'Are you sure you want to remove ${cart.cart[index].name} from the cart?'),
+                                actions: [
+                                  TextButton(
+                                    child: Text('Cancel'),
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                  ),
+                                  // TextButton(
+                                  //   child: Text('Confirm'),
+                                  //   onPressed: () {
+                                  //     cart.removeFromCart(cart.cart[index]);
+                                  //     Navigator.of(context).pop();
+                                  //   },
+                                  // ),
+                                  ElevatedButton(
+                                    onPressed: () {
+                                      cart.removeFromCart(cart.cart[index]);
+                                      Navigator.of(context).pop();
+                                      ScaffoldMessenger.of(context)
+                                          .showSnackBar(
+                                        SnackBar(
+                                          content: Text(
+                                              '${cart.cart[index].name} Deleted cart!'),
+                                          duration: Duration(seconds: 2),
+                                        ),
+                                      );
+                                    },
+                                    child: Text('Confirm'),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        },
+                      ),
+                    ],
+                  ),
                 );
               },
             ),
